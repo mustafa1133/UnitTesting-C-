534cb19431e7e945dc79c626546a37e6b8b3df17
mustafa1133 <mustafa1133@hotmail.com>
2019-03-06 23:23:34 -0500

    first commit

---
+++ BankTest/BankAccount.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BankTest
+{
+    public class BankAccount
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ UnitTestProject1/BankTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BankAccountNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BankTest
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("john Dow", beginningBalance);
+
+            account.Debit(debitAmount);
+
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+
+        }
+    }
+}
